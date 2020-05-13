be8f64c15f4a6efa17bf0186f82faacc8115245b
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-05-13 17:11:48 -0300

    Trabalhando com notificações dentro do mongodb

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -16,9 +16,18 @@ class NotificationController {
     const notifications = await Notification.find({
       user: req.userId,
     })
-      .sort('createdAt')
+      .sort({ createdAt: 'desc' })
       .limit(20);
     return res.json(notifications);
+  }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+    return res.json(notification);
   }
 }
 
